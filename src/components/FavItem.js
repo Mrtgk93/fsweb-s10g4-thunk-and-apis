df1233ecae05a55eec0f9534706d15656e465b22
Mrtgk93 <mrtgk93@hotmail.com>
2023-03-02 23:56:25 +0300

    favitem tarafına tailwind ile birkaç stil verildi

--- src/components/FavItem.js
+++ src/components/FavItem.js
@@ -6,10 +6,13 @@ function FavItem({ title }) {
   const dispatch = useDispatch();
   const { type, punchline, setup, id } = title;
   return (
-    <div className="bg-white shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
-      <p className="text-2xl p-10">{type}</p>
-      <p className="text-2xl p-10">{setup}</p>
-      <p className="text-2xl p-10">{punchline}</p>
+    <div
+      className="bg-white max-w-lg text-center hover:shadow-lg p-3 pl-5 flex flex-col
+     transition-all"
+    >
+      <p className="text-2xl p-4">{type}</p>
+      <p className="text-2xl p-4">{setup}</p>
+      <p className="text-2xl p-4">{punchline}</p>
       <button
         onClick={() => dispatch(removeFav(id))}
         className="transition-all px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"
--- src/components/Item.js
+++ src/components/Item.js
@@ -3,9 +3,9 @@ import React from "react";
 function Item({ data }) {
   return (
     <div className="shadow-md bg-white text-center">
-      <p className="text-2xl p-10">{data.type}</p>
-      <p className="text-2xl p-10">{data.setup}</p>
-      <p className="text-2xl p-10">{data.punchline}</p>
+      <p className="text-2xl p-4">{data.type}</p>
+      <p className="text-2xl p-4">{data.setup}</p>
+      <p className="text-2xl p-4">{data.punchline}</p>
     </div>
   );
 }
